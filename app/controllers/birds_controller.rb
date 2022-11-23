1656509ab8ad4573657ec747a7881060abf0c280
Kelvin M Matheka <beingmatheka@gmail.com>
2022-11-24 02:39:13 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -32,6 +32,7 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  
 
   # PATCH /birds/:id/like
   def increment_likes
@@ -39,6 +40,16 @@ class BirdsController < ApplicationController
     if bird
       bird.update(likes: bird.likes + 1)
       render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
